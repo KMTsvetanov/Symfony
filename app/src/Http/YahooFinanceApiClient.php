0d6096925e8a9b15225de0f77a80500ba51e1a99
Kiril <k.m.tsvetanov@gmail.com>
2023-11-08 14:46:53 +0200

    symfony-injecting-the-symfony-httpclient-component

--- app/src/Http/YahooFinanceApiClient.php
+++ app/src/Http/YahooFinanceApiClient.php
@@ -2,10 +2,31 @@
 
 namespace App\Http;
 
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
 class YahooFinanceApiClient
 {
-    public function fetchStockProfile($symbol, $region, $lang)
+    private const URL = 'https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-profile';
+    private const X_RAPID_API_HOST = 'apidojo-yahoo-finance-v1.p.rapidapi.com';
+
+    public function __construct(private HttpClientInterface $httpClient, private string $rapidApiKey)
     {
-        // TODO
+    }
+
+    public function fetchStockProfile($symbol, $region, $lang): array
+    {
+        $response = $this->httpClient->request('GET', self::URL, [
+            'query' => [
+                'symbol' => $symbol,
+                'region' => $region,
+                'lang' => $lang,
+            ],
+            'headers' => [
+                'X-RapidAPI-Host' => self::X_RAPID_API_HOST,
+                'X-RapidAPI-Key' => $this->rapidApiKey,
+            ],
+        ]);
+
+        dd($response->getContent());
     }
 }
